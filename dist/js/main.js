47013d738c3615b69d88c5b78f7d88feff633b4d
xJuhoo <juho.rekonen@student.lut.fi>
2024-04-16 10:26:30 +0300

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-// Select DOM Items
+// Select DOM(Document Object Models) items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-// Set Initial State Of Menu
+// Set initial state of menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -17,18 +17,18 @@ function toggleMenu() {
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
-
-        // Set Menu State
+        
+        // Set menu state
         showMenu = true;
-
-    } else {
+    }
+    else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
-
-        // Set Menu State
+        
+        // Set menu state
         showMenu = false;
     }
 }
